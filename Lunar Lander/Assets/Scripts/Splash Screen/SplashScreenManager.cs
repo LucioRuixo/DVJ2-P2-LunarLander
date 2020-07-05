4797226c5e45668859ad13fc4334f0f67b6fb383
Lucio Ruixo <lucioruixo@gmail.com>
2020-07-05 18:33:05 -0300

    Splash screens added

---
+++ Lunar Lander/Assets/Scripts/Splash Screen/SplashScreenManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenManager : MonoBehaviour
+{
+    public float fadeWaitTime;
+    public float fadeTime;
+    public float displayTime;
+    float alphaStep;
+
+    public TextMeshProUGUI aGameBy;
+    public TextMeshProUGUI companyName;
+    public TextMeshProUGUI title;
+
+    void Start()
+    {
+        alphaStep = fadeWaitTime / fadeTime;
+
+        StartCoroutine(ShowCompanyLogo(fadeWaitTime));
+    }
+
+    IEnumerator ShowCompanyLogo(float waitTime)
+    {
+        for (float t = 0f; t < fadeTime; t += waitTime)
+        {
+            aGameBy.alpha += alphaStep;
+            companyName.alpha += alphaStep;
+            yield return new WaitForSeconds(waitTime);
+        }
+
+        yield return new WaitForSeconds(displayTime);
+
+        for (float t = 0f; t < fadeTime; t += waitTime)
+        {
+            aGameBy.alpha -= alphaStep;
+            companyName.alpha -= alphaStep;
+            yield return new WaitForSeconds(waitTime);
+        }
+
+        StartCoroutine(ShowTitle(fadeWaitTime));
+    }
+
+    IEnumerator ShowTitle(float waitTime)
+    {
+        for (float t = 0f; t < fadeTime; t += waitTime)
+        {
+            title.alpha += alphaStep;
+            yield return new WaitForSeconds(waitTime);
+        }
+
+        yield return new WaitForSeconds(displayTime);
+
+        for (float t = 0f; t < fadeTime; t += waitTime)
+        {
+            title.alpha -= alphaStep;
+            yield return new WaitForSeconds(waitTime);
+        }
+
+        SceneManager.LoadScene("Main Menu");
+    }
+}
